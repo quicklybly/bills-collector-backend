baeed77c3e49b9a5859fc1c14f02b3fbe0787d02
quicklybly <quickly.tawer@gmail.com>
2024-03-30 23:26:45 +0300

    BILLS-44 Initial

---
+++ bills-collector/src/test/kotlin/com/quicklybly/billscollector/BillsCollectorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.quicklybly.billscollector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BillsCollectorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
