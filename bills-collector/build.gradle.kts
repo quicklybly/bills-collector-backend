4177f2395c33e0a75a4f9fbd957662cb50776e2e
quicklybly <quickly.tawer@gmail.com>
2024-06-23 11:48:54 +0300

    BILLS-62 add validation starter

--- bills-collector/build.gradle.kts
+++ bills-collector/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
     implementation("org.springframework.boot", "spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot", "spring-boot-starter-validation")
 
     // kotlin
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin")
